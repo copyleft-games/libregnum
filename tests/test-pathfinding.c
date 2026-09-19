cc809d9d2e0ac6d8a65b4918faecd090c270cb6b
Zach Podbielniak <z.podbielniak@disroot.org>
2026-09-18 22:36:33 -0400

    fix(pathfinding): clip grid operations and validate neighbor origins

--- tests/test-pathfinding.c
+++ tests/test-pathfinding.c
@@ -793,6 +793,79 @@ test_pathfinder_reference_maps (void)
     g_rand_free (random);
 }
 
+static void
+test_nav_grid_clipped_rectangles (void)
+{
+    const struct { gint x, y; guint width, height; } cases[] = {
+        { -2, -1, 4, 3 }, { 2, 2, G_MAXUINT, G_MAXUINT },
+        { G_MININT, G_MININT, G_MAXUINT, G_MAXUINT },
+        { G_MAXINT, G_MAXINT, G_MAXUINT, G_MAXUINT },
+        { -10, -10, 2, 2 }, { 0, 0, 0, G_MAXUINT },
+        { G_MININT, 0, 1, 3 }
+    };
+    guint i;
+
+    for (i = 0; i < G_N_ELEMENTS (cases); i++)
+    {
+        g_autoptr(LrgNavGrid) grid = lrg_nav_grid_new (4, 3);
+        gint x, y;
+
+        lrg_nav_grid_fill_rect (grid, cases[i].x, cases[i].y,
+                                cases[i].width, cases[i].height,
+                                LRG_NAV_CELL_BLOCKED, 2.5f);
+        for (y = 0; y < 3; y++)
+            for (x = 0; x < 4; x++)
+            {
+                gboolean inside = (gint64)x >= cases[i].x &&
+                    (gint64)y >= cases[i].y &&
+                    (gint64)x < (gint64)cases[i].x + cases[i].width &&
+                    (gint64)y < (gint64)cases[i].y + cases[i].height;
+
+                g_assert_cmpint (lrg_nav_grid_is_walkable (grid, x, y), ==, !inside);
+                g_assert_cmpfloat (lrg_nav_grid_get_cell_cost (grid, x, y), ==,
+                                   inside ? 2.5f : 1.0f);
+            }
+    }
+}
+
+static void
+test_nav_grid_invalid_fill_cost (void)
+{
+    g_autoptr(LrgNavGrid) grid = lrg_nav_grid_new (2, 2);
+    const gfloat costs[] = { -1.0f, NAN };
+    guint i;
+    gint x, y;
+
+    for (i = 0; i < G_N_ELEMENTS (costs); i++)
+    {
+        g_test_expect_message (NULL, G_LOG_LEVEL_CRITICAL, "*cost >= 0.0f*");
+        lrg_nav_grid_fill_rect (grid, 0, 0, 2, 2, LRG_NAV_CELL_BLOCKED, costs[i]);
+        g_test_assert_expected_messages ();
+        for (y = 0; y < 2; y++)
+            for (x = 0; x < 2; x++)
+            {
+                g_assert_true (lrg_nav_grid_is_walkable (grid, x, y));
+                g_assert_cmpfloat (lrg_nav_grid_get_cell_cost (grid, x, y), ==, 1.0f);
+            }
+    }
+}
+
+static void
+test_nav_grid_invalid_neighbor_origin (void)
+{
+    g_autoptr(LrgNavGrid) grid = lrg_nav_grid_new (3, 3);
+    const gint origins[][2] = { {-1, 0}, {0, -1}, {3, 0}, {0, 3},
+        {G_MININT, 0}, {0, G_MININT}, {G_MAXINT, 0}, {0, G_MAXINT} };
+    guint i;
+    GList *neighbors;
+
+    for (i = 0; i < G_N_ELEMENTS (origins); i++)
+        g_assert_null (lrg_nav_grid_get_neighbors (grid, origins[i][0], origins[i][1]));
+    neighbors = lrg_nav_grid_get_neighbors (grid, 0, 0);
+    g_assert_cmpuint (g_list_length (neighbors), ==, 3);
+    g_list_free_full (neighbors, (GDestroyNotify)lrg_nav_cell_free);
+}
+
 int
 main (int   argc,
       char *argv[])
@@ -842,6 +915,10 @@ main (int   argc,
     g_test_add_func ("/pathfinding/pathfinder/reference-maps", test_pathfinder_reference_maps);
     g_test_add_func ("/pathfinding/error-enum", test_pathfinding_error_enum);
     g_test_add_func ("/pathfinding/heuristics-large", test_heuristics_large_coordinates);
+
+    g_test_add_func ("/pathfinding/nav-grid/clipped-rectangles", test_nav_grid_clipped_rectangles);
+    g_test_add_func ("/pathfinding/nav-grid/invalid-fill-cost", test_nav_grid_invalid_fill_cost);
+    g_test_add_func ("/pathfinding/nav-grid/invalid-neighbor-origin", test_nav_grid_invalid_neighbor_origin);
 
     /* Heuristic tests */
     g_test_add_func ("/pathfinding/heuristics", test_heuristics);
